0057c9f7b25eb7def8cbdf2be54924ac4ebbcd6d
SlaterLatiao <41390071+SlaterLatiao@users.noreply.github.com>
2022-05-02 00:09:13 -0500

    generate patch for the remaining benchmarks

---
+++ benchmarks/libxml2_xml/target.cc
@@ -0,0 +1,68 @@
+#ifdef FRCOV
+#define FIXREVERTER_SIZE 5516
+short FIXREVERTER[FIXREVERTER_SIZE];
+#endif
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+#include <vector>
+#include "libxml/xmlversion.h"
+#include "libxml/parser.h"
+#include "libxml/HTMLparser.h"
+#include "libxml/tree.h"
+
+void ignore (void * ctx, const char * msg, ...) {}
+
+
+#ifdef FRCOV
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#endif
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  
+  #ifdef FRCOV
+  char *fixReverterEnv = getenv("FIXREVERTER");
+  char *fixReverterToken = strtok(fixReverterEnv, " ");
+  if (fixReverterToken == NULL) {
+    for (int i = 0; i < FIXREVERTER_SIZE; i++)
+      FIXREVERTER[i] = 1;
+  } else if (!strcmp("on", fixReverterToken)) {
+    for (int i = 0; i < FIXREVERTER_SIZE; i++)
+      FIXREVERTER[i] = 0;
+    fixReverterToken = strtok(NULL, " ");
+    while (fixReverterToken != NULL) {
+      FIXREVERTER[atoi(fixReverterToken)] = 1;
+      fixReverterToken = strtok(NULL, " ");
+    }
+  } else if (!strcmp("off", fixReverterToken)) {
+    for (int i = 0; i < FIXREVERTER_SIZE; i++)
+      FIXREVERTER[i] = 1;
+    fixReverterToken = strtok(NULL, " ");
+    while (fixReverterToken != NULL) {
+      FIXREVERTER[atoi(fixReverterToken)] = 0;
+      fixReverterToken = strtok(NULL, " ");
+    }
+  } else {
+    fprintf(stderr, "[FIXREVERTER] - first token must be on or off\n");
+    exit(0);
+  }
+  #endif
+  xmlSetGenericErrorFunc(NULL, &ignore);
+  if (auto doc = xmlReadMemory(reinterpret_cast<const char *>(data), size,
+                               "noname.xml", NULL, 0))
+    xmlFreeDoc(doc);
+  return 0;
+}
---
+++ benchmarks/usrsctp/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -eu
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+git apply ../fr_injection.patch
+# add FRCOV flag if building coverage
+if [[ -z "${FR_COV_BUILD}" ]]; then
+  cmake -Dsctp_build_programs=0 -Dsctp_debug=0 -Dsctp_invariants=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo .
+else
+  cmake -Dsctp_build_programs=0 -Dsctp_debug=0 -Dsctp_invariants=1 -DCMAKE_C_FLAGS=-DFRCOV -DCMAKE_CXX_FLAGS=-DFRCOV -DCMAKE_BUILD_TYPE=RelWithDebInfo .
+fi
+
+make
+cd fuzzer
+
+for target in "fuzzer_connect"; do
+  $CC $CFLAGS -DFUZZING_STAGE=0 -I . -I ../usrsctplib/ -c ${target}.c -o $OUT/${target}.o
+  $CXX $CXXFLAGS -o $OUT/${target} $OUT/${target}.o $LIB_FUZZING_ENGINE ../usrsctplib/libusrsctp.a
+  rm -f $OUT/${target}.o
+done
+
+zip -jr fuzzer_connect_seed_corpus.zip CORPUS_CONNECT/
+cp fuzzer_connect_seed_corpus.zip $OUT/
