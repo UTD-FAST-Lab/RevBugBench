dd94d492ca9defd37e755d8a456e40d393a2b083
SlaterLatiao <41390071+SlaterLatiao@users.noreply.github.com>
2022-11-07 17:41:49 -0600

    format.

--- triage/setup/setup.py
+++ triage/setup/setup.py
@@ -7,3 +7,4 @@ def setup(helper: ConfigHelper) -> None:
     exp_tuples = precheck.exp_tuples(helper.benchmarks(), helper.fuzzers(), helper.exps(), helper.raw_data_dir())
     for benchmark, fuzzer, exp_name in exp_tuples:
         extract_fuzzing_result(benchmark, fuzzer, exp_name, helper)
+        
